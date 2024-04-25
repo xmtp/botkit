dfb6b6e59131c3a3b7aaa368a44d2352193b4626
fabri <fguespe@gmail.com>
2024-04-25 20:09:12 -0300

    update

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,3 @@
-import "dotenv/config";
-import HandlerContext from "./lib/handler-context";
-import run from "./lib/runner.js";
-
-run(async (context: HandlerContext) => {
-  const messageBody = context.message.content;
-
-  // To reply, just call `reply` on the HandlerContext.
-  await context.reply(`ECHO: ${messageBody}`);
-});
-
 export { default as createClient } from "./lib/client.js";
 export { default as run } from "./lib/runner.js";
 export { default as HandlerContext } from "./lib/handler-context.js";
