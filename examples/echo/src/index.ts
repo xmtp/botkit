f8f1ec2b82d3b6983fadabbd1f8c2c52f4461a05
fabri <fguespe@gmail.com>
2024-04-26 01:43:39 -0300

    update

--- examples/echo/src/index.ts
+++ examples/echo/src/index.ts
@@ -1,8 +1,10 @@
 import "dotenv/config";
-import { xmtpClient, run, HandlerContext } from "@xmtp/botkit";
+import { run, HandlerContext } from "@xmtp/botkit";
 
 run(async (context: HandlerContext) => {
+  // Get the message and the address from the sender
   const { content, senderAddress } = context.message;
 
+  // To reply, just call `reply` on the HandlerContext.
   await context.reply(`ECHO: ${content}`);
 });
